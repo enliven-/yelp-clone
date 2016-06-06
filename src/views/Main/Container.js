14502198986db58cd2d17e8328d60acd601b3c92
Viksit Arora <viksit@mit.edu>
2016-06-06 15:52:19 -0400

    Google Map component added; api_key is hard coded, fix later

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Map, {GoogleApiWrapper} from 'google-maps-react';
 
-export default class Container extends React.Component {
+class Container extends React.Component {
   render() {
     return (
       <div>
         Hello from the container
+        <Map
+          google={this.props.google} />
       </div>
     )
   }
 }
 
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
+})(Container);
