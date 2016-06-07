146a2037e3567b6d0f047cf404d501cce8d5b32b
Viksit Arora <viksit@mit.edu>
2016-06-07 10:33:11 -0400

    added Rating component; Item now uses Rating

--- src/components/Listing/Item.js
+++ src/components/Listing/Item.js
@@ -1,6 +1,6 @@
 import React, { PropTypes as T } from 'react'
 import classnames from 'classnames'
-
+import Rating from 'components/Rating/Rating';
 import styles from './styles.module.css'
 
 export default class Item extends React.Component {
@@ -10,7 +10,8 @@ export default class Item extends React.Component {
       <div
         className={styles.item}>
           <h1 className={classnames(styles.title)}>{place.name}</h1>
-          <span>{place.rating/5}</span>
+          <Rating className={styles.rating}
+                  percentage={(place.rating/5)} />
       </div>
     )
   }
