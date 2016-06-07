6785a453c71e52d6529780816db75979a8122d29
Viksit Arora <viksit@mit.edu>
2016-06-07 12:45:27 -0400

    added markers to Map

--- src/views/Main/Map/Map.js
+++ src/views/Main/Map/Map.js
@@ -1,16 +1,30 @@
 import React, { PropTypes as T } from 'react'
 import classnames from 'classnames'
-import Map from 'google-maps-react'
+import Map, { Marker } from 'google-maps-react'
 
 import styles from './styles.module.css'
 
 export default class MapComponent extends React.Component {
+
+  renderMarkers() {
+    if (!this.props.places) { return null; }
+    return this.props.places.map(place =>{
+      return <Marker key={place.id}
+                name={place.id}
+                place={place}
+                position={place.geometry.location}
+              />
+    })
+  }
+
   render() {
     return (
       <Map
         google={this.props.google}
         className={styles.map}
-      />
+      >
+        {this.renderMarkers()}
+      </Map>
     )
   }
 }
